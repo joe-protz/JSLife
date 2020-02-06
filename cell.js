ae0f9f83b38da4b3692600aa16cc9df27c0717aa
joe10994 <joe10994@gmail.com>
2020-02-06 13:29:11 -0500

    Add documentation and first attempt at finding neighbors

--- cell.js
+++ cell.js
@@ -7,14 +7,36 @@ class Cell {
     this.x = x
     this.y = y
   }
+  // have count of alive neighbors that must be equal to 2 or 3 to stay alive or if 3 live neighbors will come alive
+  getNeighbors() {
+    // The formula to find a vector and a one dimensional array is X plus Y times columns 
 
-  // update () {
+    // THIS IS NOT RIGHT
+    this.neighbors = [
+      cells[(((this.x - scale) + (this.y - scale)) * (width / scale))],
 
-  // }
+      cells[((this.x + (this.y - scale)) * (width / scale))],
+
+      cells[(((this.x + scale) + (this.y - scale)) * (width / scale))],
+
+      cells[(((this.x - scale) + this.y) * (width / scale))],
+
+      cells[(((this.x + scale) + this.y) * (width / scale))],
+
+      cells[(((this.x - scale) + (this.y + scale)) * (width / scale))],
+
+      cells[((this.x + (this.y + scale)) * (width / scale))],
+      cells[(((this.x + scale) + (this.y + scale)) * (width / scale))],
+
+
+    ]
+    console.log(this.neighbors)
+  }
+  // just changes alive state
   click() {
     this.alive = true
   }
-
+  // shows a rectangle (square) at the location and red if dead, green if alive
   show() {
     if (this.alive) {
       fill(0, 255, 0)
--- sketch.js
+++ sketch.js
@@ -1,36 +1,54 @@
+// holds all cells for the board
 let cells = []
-let scale = 10
+// scale to make the cells larger/smaller
+let scale = 100
+// have I clicked start?
 let start = false
 
 function setup() {
 
   frameRate(10)
-  createCanvas(600, 300);
+  createCanvas(300, 300);
+
   button = createButton('start');
   button.position(30, height + 30);
-  button.mousePressed(() => start = true);
-  for (i = 0; i < width / scale; i++) {
-    for (j = 0; j < height / scale; j++) {
-      cells.push(new Cell(i * scale, j * scale, scale))
+  // starts the game
+  button.mousePressed(() => start = true)
+  // creates new cells for the board and pushes them into cells in order
+  for (i = 0; i < (width / scale); i++) {
+    for (j = 0; j < (height / scale); j++) {
+      cells.push(new Cell(j * scale, i * scale, scale))
+
     }
   }
 }
-
+// continuously loops @ framerate
 function draw() {
 
   background(62)
-  cells.forEach((cell, index) => {
-    if (mouseIsPressed) {
+  // loop for setting game up
+  if (!start) {
 
-      if (dist(mouseX - scale / 2, mouseY - scale / 2, cells[index].x, cells[index].y) < scale / 2) {
-        cells[index].click()
+    cells.forEach((cell, index) => {
+      // when a pressed mouse is over a cell, change it to alive
+      if (mouseIsPressed) {
+        if (dist(mouseX - scale / 2, mouseY - scale / 2, cells[index].x, cells[index].y) < scale / 2) {
+          cells[index].click()
+        }
       }
-    }
-    cells[index].show()
+      //show all cells 
+      cells[index].show()
 
-  })
+    })
+  }
+  // loop for once the game is setup and button has been pressed
   if (start) {
-
+    cells.forEach((cell, index) => {
+      cells[index].show()
+      // getNeighbors is needed to use the rules. doesn't yet work.
+      cells[index].getNeighbors()
+    })
+    noLoop() // this stops the draw loop, just there for debugging
   }
 
 }
