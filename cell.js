39feb8b6db59445e4c25954b3d3d7e60d748a7d6
joe10994 <joe10994@gmail.com>
2020-02-06 09:52:59 -0500

    Create cell class and store them in an array of Cells
    Create cell with x,y, and scale
    show a rect in the position of each cell with show method
    delete un-needed sound file

--- cell.js
+++ cell.js
@@ -1,1 +1,20 @@
 'use strict'
+
+class Cell {
+  constructor(x, y, scale) {
+    this.alive = false
+    this.scale = scale
+    this.x = x
+    this.y = y
+
+  }
+
+  // update = function () {
+
+  // }
+
+  show() {
+    fill(255)
+    rect(this.x, this.y, scale, scale)
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let cells = []
+let scale = 10
+
 function setup() {
   createCanvas(640, 480);
+  for (i = 0; i < width / scale; i++) {
+    for (j = 0; j < height / scale; j++) {
+      cells.push(new Cell(i * scale, j * scale, scale))
+    }
+  }
 }
 
 function draw() {
+  background(62)
+  cells.forEach((cell, index) => cells[index].show())
+
 
 }
