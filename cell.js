8806534802be6eeb78fcbe9d7b0db03adad34a27
joe10994 <joe10994@gmail.com>
2020-02-06 21:17:15 -0500

    Delete multiple unneeeded duplicate arrays
    Create getAliveNeighbors function
    Move getNeighbors into setup function where it is only called once.
    Create conways rules in draw function.
    Was using maps of the cells array to avoid mutating it when checking
    various rules, worked out logic to not need them.
    Add comments for each method.

--- cell.js
+++ cell.js
@@ -28,11 +28,7 @@ class Cell {
   // have count of alive this.neighbors that must be equal to 2 or 3 to stay alive or if 3 live this.neighbors will come alive
   getNeighbors() {
     // The formula to find a vector and a one dimensional array is X plus Y times columns 
-
-
-    while (arrMap.length) coordArray.push(arrMap.splice(0, width / scale))
-
-
+    // we fall back to its neighbor being undefined if we would be checking off of the grid
     if (this.touchesTop || this.touchesLeft) {
       this.neighbors[0] = undefined
     } else {
@@ -48,48 +44,50 @@ class Cell {
     if (this.touchesTop || this.touchesRight) {
       this.neighbors[2] = undefined
     } else {
-    this.neighbors[2] = cells[((this.x + scale) / scale) + ((this.y - scale) / scale) * (width / scale)]
+      this.neighbors[2] = cells[((this.x + scale) / scale) + ((this.y - scale) / scale) * (width / scale)]
     }
 
-     if (this.touchesLeft) {
-       this.neighbors[3] = undefined
-     } else {
+    if (this.touchesLeft) {
+      this.neighbors[3] = undefined
+    } else {
       this.neighbors[3] = cells[(this.x - scale) / scale + (this.y / scale) * (width / scale)]
-     }
+    }
 
-     if (this.touchesRight) {
-       this.neighbors[4] = undefined
-     } else {
-     this.neighbors[4]  = cells[(this.x + scale) / scale + (this.y / scale) * (width / scale)]
-     }
+    if (this.touchesRight) {
+      this.neighbors[4] = undefined
+    } else {
+      this.neighbors[4] = cells[(this.x + scale) / scale + (this.y / scale) * (width / scale)]
+    }
 
-     if (this.touchesBottom || this.touchesLeft) {
-       this.neighbors[5] = undefined
-     } else {
+    if (this.touchesBottom || this.touchesLeft) {
+      this.neighbors[5] = undefined
+    } else {
       this.neighbors[5] = cells[(this.x - scale) / scale + ((this.y + scale) / scale) * (width / scale)]
-     }
+    }
 
-     if(this.touchesBottom) {
-       this.neighbors[6]  = undefined
-     } else {
+    if (this.touchesBottom) {
+      this.neighbors[6] = undefined
+    } else {
       this.neighbors[6] = cells[this.x / scale + ((this.y + scale) / scale) * (width / scale)]
-     }
+    }
 
-     if (this.touchesBottom || this.touchesRight) {
-       this.neighbors[7] = undefined
-     } else{
-     this.neighbors[7] = cells[(this.x + scale) / scale + ((this.y + scale) / scale) * (width / scale)]
-}
-
-
-
-      for (let i = 0; i < this.neighbors.length; i++) {
-        if (this.neighbors[i] !== undefined && this.neighbors[i].alive) {
-          this.aliveNeighbors.push(this.neighbors[i])
-        }
-      }
+    if (this.touchesBottom || this.touchesRight) {
+      this.neighbors[7] = undefined
+    } else {
+      this.neighbors[7] = cells[(this.x + scale) / scale + ((this.y + scale) / scale) * (width / scale)]
+    }
   }
-  // just changes alive state
+
+  // empty the array and then loop through the neighbors and push the alive ones 
+  getAliveNeighbors() {
+    this.aliveNeighbors = new Array()
+    for (let i = 0; i < this.neighbors.length; i++) {
+      if (this.neighbors[i] !== undefined && this.neighbors[i].alive) {
+        this.aliveNeighbors.push(this.neighbors[i])
+      }
+    }
+  }
+  // just changes alive state during setup if we click on a cell
   click() {
     this.alive = true
   }
@@ -100,6 +98,7 @@ class Cell {
     } else {
       fill(255, 0, 0)
     }
+    // starts at top left corner of rect of size scale on each side
     rect(this.x, this.y, scale, scale)
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,29 +1,37 @@
 // holds all cells for the board
 let cells = []
+//may use at a later date, holds 2d array of cells
 let coordArray = []
 // scale to make the cells larger/smaller
-let scale = 100
+let scale = 50
 // have I clicked start?
 let start = false
-let arrMap
+let arrMap // a copy of cells , used to make coordArray
+
+
 
 function setup() {
 
-  frameRate(10)
-  createCanvas(300, 300);
+  frameRate(5)
+  createCanvas(500, 500);
 
   button = createButton('start');
   button.position(30, height + 30);
   // starts the game
   button.mousePressed(() => start = true)
-  // creates new cells for the board and pushes them into cells in order
-  for (i = 0; i < (width / scale); i++) {
-    for (j = 0; j < (height / scale); j++) {
+  // creates new cells for the board and pushes them into cells in order of creation
+  for (let i = 0; i < (width / scale); i++) {
+    for (let j = 0; j < (height / scale); j++) {
       cells.push(new Cell(j * scale, i * scale, scale))
-
     }
   }
+  // initiate the cells neighbors array so they all know their neighbors
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].getNeighbors()
+  }
+  //make a coordArray, may be used at later date
   arrMap = cells.map(cell => cell)
+  while (arrMap.length) coordArray.push(arrMap.splice(0, width / scale))
 }
 // continuously loops @ framerate
 function draw() {
@@ -41,17 +49,30 @@ function draw() {
       }
       //show all cells 
       cells[index].show()
-
     })
   }
+
   // loop for once the game is setup and button has been pressed
   if (start) {
+    // create an aliveNeighbors array , we get the alive neighbors BEFORE checking so that we don't mess with the results before checking each one
     cells.forEach((cell, index) => {
-      cells[index].show()
-      // getNeighbors is needed to use the rules. doesn't yet work.
-      cells[index].getNeighbors()
+      cells[index].getAliveNeighbors()
     })
-    noLoop() // this stops the draw loop, just there for debugging
+    // conways rules for the game
+    cells.forEach((cell, index) => {
+      if (cell.alive) {
+        if (cell.aliveNeighbors.length < 2) {
+          cell.alive = false
+        }
+        if (cell.aliveNeighbors.length > 3) {
+          cell.alive = false
+        }
+      } else {
+        if (cell.aliveNeighbors.length === 3) {
+          cell.alive = true
+        }
+      }
+      cell.show()
+    })
   }
-
 }
