38dc499a041d523e1bd8f8b32d7dbc44d8e9a036
joe10994 <joe10994@gmail.com>
2020-02-09 16:14:46 -0500

    Major refactor and add in 'gun' button

--- cell.js
+++ cell.js
@@ -1,117 +1,41 @@
 'use strict'
 
 class Cell {
-  constructor(x, y, scale) {
-    this.alive = false
-    this.scale = scale
-    this.x = x
-    this.y = y
-    this.neighbors = []
-    this.aliveNeighbors = []
+  constructor(i, j) {
+    this.i = i
+    this.j = j
+    this.alive = 0
+    this.x = i * scale
+    this.y = j * scale
     this.hasBeenChanged = false
-    if (this.x === 0) {
-      this.touchesLeft = true
-    }
-    if (this.x === width - scale) {
-      this.touchesRight = true
-    }
-    if (this.y === 0) {
-      this.touchesTop = true
-    }
-    if (this.y === height - scale) {
-      this.touchesBottom = true
-    }
+    this.aliveNeighbors = 0
   }
 
   // have count of alive this.neighbors that must be equal to 2 or 3 to stay alive or if 3 live this.neighbors will come alive
   getNeighbors() {
     // The formula to find a vector and a one dimensional array is X plus Y times columns
     // we fall back to its neighbor being undefined if we would be checking off of the grid
-    if (this.touchesTop || this.touchesLeft) {
-      this.neighbors[0] = undefined
-    } else {
-      this.neighbors[0] =
-        cells[
-          (this.x - scale) / scale +
-            ((this.y - scale) / scale) * (width / scale)
-        ]
-    }
-
-    if (this.touchesTop) {
-      this.neighbors[1] = undefined
-    } else {
-      this.neighbors[1] =
-        cells[this.x / scale + ((this.y - scale) / scale) * (width / scale)]
-    }
-
-    if (this.touchesTop || this.touchesRight) {
-      this.neighbors[2] = undefined
-    } else {
-      this.neighbors[2] =
-        cells[
-          (this.x + scale) / scale +
-            ((this.y - scale) / scale) * (width / scale)
-        ]
-    }
-
-    if (this.touchesLeft) {
-      this.neighbors[3] = undefined
-    } else {
-      this.neighbors[3] =
-        cells[(this.x - scale) / scale + (this.y / scale) * (width / scale)]
-    }
-
-    if (this.touchesRight) {
-      this.neighbors[4] = undefined
-    } else {
-      this.neighbors[4] =
-        cells[(this.x + scale) / scale + (this.y / scale) * (width / scale)]
-    }
-
-    if (this.touchesBottom || this.touchesLeft) {
-      this.neighbors[5] = undefined
-    } else {
-      this.neighbors[5] =
-        cells[
-          (this.x - scale) / scale +
-            ((this.y + scale) / scale) * (width / scale)
-        ]
-    }
-
-    if (this.touchesBottom) {
-      this.neighbors[6] = undefined
-    } else {
-      this.neighbors[6] =
-        cells[this.x / scale + ((this.y + scale) / scale) * (width / scale)]
-    }
-
-    if (this.touchesBottom || this.touchesRight) {
-      this.neighbors[7] = undefined
-    } else {
-      this.neighbors[7] =
-        cells[
-          (this.x + scale) / scale +
-            ((this.y + scale) / scale) * (width / scale)
-        ]
-    }
-  }
-
-  // empty the array and then loop through the neighbors and push the alive ones
-  getAliveNeighbors() {
-    this.aliveNeighbors = new Array()
-    for (let i = 0; i < this.neighbors.length; i++) {
-      if (this.neighbors[i] !== undefined && this.neighbors[i].alive) {
-        this.aliveNeighbors.push(this.neighbors[i])
+    let total = 0
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        total +=
+          cells[(this.i + i + cols) % cols][(this.j + j + rows) % rows].alive
       }
     }
+    this.aliveNeighbors = total
+    if (this.alive === 1) {
+      this.aliveNeighbors--
+    }
   }
+
   // just changes alive state during setup if we click on a cell
   click() {
     if (
       dist(mouseX - scale / 2, mouseY - scale / 2, this.x, this.y) <
       scale / 2
     ) {
-      this.alive = true
+      console.log(this)
+      this.alive = 1
       this.hasBeenChanged = true
     }
   }
@@ -119,9 +43,9 @@ class Cell {
   show() {
     if (!this.hasBeenChanged) {
       fill(0, 0, 255)
-    } else if (this.alive) {
+    } else if (this.alive === 1) {
       fill(0, 255, 0)
-    } else if (!this.alive) {
+    } else if (this.alive === 0) {
       fill(255, 0, 0)
     }
     // starts at top left corner of rect of size scale on each side
--- sketch.js
+++ sketch.js
@@ -1,69 +1,74 @@
-let acornButton, randomSlider, resetButton, randomButton, startButton
+let acornButton, randomSlider, resetButton, randomButton, startButton, gunButton
 // holds all cells for the board
-let cells = []
+let cells
 //may use at a later date, holds 2d array of cells
-let coordArray = []
+//let coordArray = []
 // scale to make the cells larger/smaller
-let scale = 5
+let scale = 10
+let cols, rows
+
 // have I clicked start?
 let start = false
-let arrMap // a copy of cells , used to make coordArray
+//let arrMap // a copy of cells , used to make coordArray
 let randomValue
+
 const createAcornButton = () => {
   acornButton = createButton('acorn')
-  acornButton.position(110, height + 10)
+  acornButton.position(width - 400, height + 10)
   acornButton.size(80)
   acornButton.mousePressed(() => {
     let acorns = [
-      cells[6164],
-      cells[6163],
-      cells[6162],
-      cells[6061],
-      cells[5959],
-      cells[6159],
-      cells[6158]
+      cells[35][25],
+      cells[36][25],
+      cells[37][25],
+      cells[38][24],
+      cells[40][23],
+      cells[40][25],
+      cells[41][25]
     ]
     acorns.forEach(acorn => {
-      acorn.alive = true
+      acorn.alive = 1
       acorn.hasBeenChanged = true
     })
   })
 }
 const createResetButton = () => {
   resetButton = createButton('reset')
-  resetButton.position(30, height + 50)
+  resetButton.position(width-500, height + 50)
   resetButton.size(80)
   resetButton.mousePressed(() => {
-    noLoop()
-    randomSlider.remove()
+    //noLoop()
+    //randomSlider.remove()
     cells = []
     start = false
-    setup()
-    loop()
+    resetSketch()
+    //loop()
   })
 }
 const createRandomSlider = () => {
-  randomSlider = createSlider(0.1, 0.9, 0.3, 0.1)
-  randomSlider.position(110, height + 30)
+  randomSlider = createSlider(0.1, 0.9, 0.3, 0.05)
+  randomSlider.position(width-400, height + 30)
   randomSlider.style('width', '80px')
 }
 const createRandomButton = () => {
   randomButton = createButton('random')
-  randomButton.position(30, height + 30)
+  randomButton.position(width-500, height + 30)
   randomButton.size(80)
   // randoms the game
   randomButton.mousePressed(() => {
-    cells.forEach(cell => {
-      if (random() < randomValue) {
-        cell.alive = true
-        cell.hasBeenChanged = true
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        if (random() < randomValue) {
+          cells[i][j].alive = 1
+          cells[i][j].hasBeenChanged = true
+        }
       }
-    })
+    }
   })
 }
 const createStartButton = () => {
   startButton = createButton('start')
-  startButton.position(30, height + 10)
+  startButton.position(width-500, height + 10)
   startButton.size(80)
   // starts the game
   startButton.mousePressed(() => (start = true))
@@ -72,44 +77,117 @@ const createPageElements = function() {
   let args = Array.prototype.slice.call(arguments)
   args.forEach(callback => callback())
 }
+const createGunButton = () => {
+  gunButton = createButton('gun')
+  gunButton.position(width-400, height + 50)
+  gunButton.size(80)
+  gunButton.mousePressed(() => {
+    let guns = [
+      cells[26][25],
+      cells[27][25],
+      cells[26][26],
+      cells[27][26],
+
+      cells[36][26],
+      cells[36][25],
+      cells[36][27],
+
+      cells[37][24],
+      cells[37][28],
+      cells[38][23],
+      cells[38][29],
+
+      cells[39][23],
+      cells[39][29],
+
+      cells[40][26],
+
+      cells[41][28],
+      cells[41][24],
+
+      cells[42][27],
+      cells[42][26],
+      cells[42][25],
+
+      cells[43][26],
+
+      cells[46][24],
+      cells[46][23],
+      cells[46][25],
+
+      cells[47][25],
+      cells[47][24],
+      cells[47][23],
+
+      cells[48][22],
+      cells[48][26],
+
+      cells[50][22],
+      cells[50][26],
+      cells[50][21],
+      cells[50][27],
+
+      cells[60][24],
+      cells[60][23],
+      cells[61][24],
+      cells[61][23]
+    ]
+    guns.forEach(gun => {
+      gun.alive = 1
+      gun.hasBeenChanged = true
+    })
+  })
+}
+
+const resetSketch = () => {
+  cells = new Array(width / scale)
+  for (i = 0; i < cells.length; i++) {
+    cells[i] = new Array(height / scale)
+  }
+  // creates new cells for the board and pushes them into cells in order of creation
+  for (let i = 0; i < width / scale; i++) {
+    for (let j = 0; j < height / scale; j++) {
+      cells[i][j] = new Cell(i, j)
+    }
+  }
+
+  // initiate the cells neighbors array so they all know their neighbors
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      cells[i][j].getNeighbors()
+    }
+  }
+}
 function setup() {
   textSize(50)
-  createCanvas(500, 500)
+  createCanvas(800, 500)
+  cols = width / scale
+  rows = height / scale
   createPageElements(
     createResetButton,
     createRandomSlider,
     createRandomButton,
     createAcornButton,
-    createStartButton
+    createStartButton,
+    createGunButton
   )
-
-  // creates new cells for the board and pushes them into cells in order of creation
-  for (let i = 0; i < width / scale; i++) {
-    for (let j = 0; j < height / scale; j++) {
-      cells.push(new Cell(j * scale, i * scale))
-    }
-  }
-  // initiate the cells neighbors array so they all know their neighbors
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].getNeighbors()
-  }
-  //make a coordArray, may be used at later date
-  arrMap = cells.map(cell => cell)
-  while (arrMap.length) coordArray.push(arrMap.splice(0, width / scale))
+  resetSketch()
 }
 
 function mouseDragged() {
-  cells.forEach((cell, index) => {
-    // when a pressed mouse is over a cell, change it to alive
-    cells[index].click()
-  })
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      cells[i][j].click()
+    }
+  }
 }
 
 function mouseClicked() {
-  cells.forEach((cell, index) => {
-    // when a pressed mouse is over a cell, change it to alive
-    cells[index].click()
-  })
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      cells[i][j].click()
+    }
+  }
 }
 
 // continuously loops @ framerate
@@ -118,33 +196,42 @@ function draw() {
   background(62)
   // loop for setting game up
   if (!start) {
-    cells.forEach(cell => cell.show())
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        cells[i][j].show()
+      }
+    }
   }
 
   // loop for once the game is setup and startButton has been pressed
   if (start) {
-    // create an aliveNeighbors array , we get the alive neighbors BEFORE checking so that we don't mess with the results before checking each one
-    cells.forEach((cell, index) => {
-      cells[index].getAliveNeighbors()
-    })
-    // conways rules for the game
-    cells.forEach((cell, index) => {
-      if (cell.alive) {
-        if (cell.aliveNeighbors.length < 2) {
-          cell.alive = false
-          cell.hasBeenChanged = true
-        }
-        if (cell.aliveNeighbors.length > 3) {
-          cell.alive = false
-          cell.hasBeenChanged = true
-        }
-      } else {
-        if (cell.aliveNeighbors.length === 3) {
-          cell.alive = true
-          cell.hasBeenChanged = true
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        cells[i][j].getNeighbors()
+      }
+    }
+
+    // Conway's rules for the game
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        let currentCell = cells[i][j]
+        let aliveNeighbors = currentCell.aliveNeighbors
+        if (
+          currentCell.alive === 1 &&
+          (aliveNeighbors < 2 || aliveNeighbors > 3)
+        ) {
+          currentCell.alive = 0
+          currentCell.hasBeenChanged = true
+        } else if (currentCell.alive === 0 && aliveNeighbors === 3) {
+          currentCell.alive = 1
+          currentCell.hasBeenChanged = true
         }
       }
-      cell.show()
-    })
+    }
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        cells[i][j].show()
+      }
+    }
   }
 }
